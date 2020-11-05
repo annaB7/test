996561d60c9bf9efd711ec39b1c39ff75e0f89e6
hvl1610 <hvl1610@gepplus.com>
2020-11-05 13:25:03 +0900

    완료건 폴더명 변경

--- scroll/scroll_width.js
+++ scroll/scroll_width.js
@@ -1,4 +1,8 @@
 // 스크롤에 따른 요소의 넓이를 조절해보자
 $(document).ready(function(){
-    
+    var document_h = document.outerHeight;
+    console.log(document_h);
+    $(document).scroll(function(){
+
+    });
 });
