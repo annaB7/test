ccdbfa2ba01a800cf9e7b7829cf34e0abf131dff
kimanna <vijel@naver.com>
2020-11-11 19:37:31 +0900

    no message

--- ball/ball.js
+++ ball/ball.js
@@ -46,6 +46,7 @@ export class Ball {
         const maxX = block.maxX + this.radius;
         const minY = block.y - this.radius;
         const maxY = block.maxY + this.radius;
+        
 
         if(this.x > minX && this.x < maxX && this.y > minY && this.y < maxY){
             const x1 = Math.abs(minX - this.x);
